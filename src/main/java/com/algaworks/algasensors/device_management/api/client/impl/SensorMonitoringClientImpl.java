f5dae107af47d70bf27deefff645ead15daa6277
pinto.breno <breno.pedro.a.pinto@accenture.com>
2025-12-16 16:13:59 -0300

    created error treatment for gateway communication

--- src/main/java/com/algaworks/algasensors/device_management/api/client/impl/SensorMonitoringClientImpl.java
+++ src/main/java/com/algaworks/algasensors/device_management/api/client/impl/SensorMonitoringClientImpl.java
@@ -1,14 +1,25 @@
 package com.algaworks.algasensors.device_management.api.client.impl;
 
 import com.algaworks.algasensors.device_management.api.client.SensorMonitoringClient;
+import com.algaworks.algasensors.device_management.api.client.SensorMonitoringClientBadGatewayException;
 import io.hypersistence.tsid.TSID;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClient;
 
 @Component
 public class SensorMonitoringClientImpl implements SensorMonitoringClient {
 
-    private final RestClient restClient = RestClient.create("http://localhost:8082");
+    private final RestClient restClient;
+
+    public SensorMonitoringClientImpl() {
+        restClient = RestClient.builder().baseUrl("http://localhost:8082")
+                .defaultStatusHandler(HttpStatusCode::isError,
+                        (request, response) -> {
+                            throw new SensorMonitoringClientBadGatewayException();
+                        })
+                .build();
+    }
 
     @Override
     public void enableMonitoring(TSID sensorId) {
